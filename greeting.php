5d8e1f3a4690d06d6fe8f3cfa128a5477c91d0d0
SDemonteverde <38735789+Shyed@users.noreply.github.com>
2026-05-12 12:38:03 -0400

    Update greeting.php

--- greeting.php
+++ greeting.php
@@ -1,14 +1,28 @@
+/*
+PROGRAM: Welcome Message Form
+This PHP script displays a simple HTML form
+that asks the user to enter their name.
+
+After submission, the program displays
+a personalized welcome message.
+*/
+
 <?php
+
 // Printing a welcome message
 if ($_POST['user_name']) {
   print("Welcome, ");
+
+  // Display entered username
   print($_POST['user_name']);
 } else {
-  // Printing form
+  /*-- DISPLAY INPUT FORM --*/
   echo "<FORM method='post' action='".$_SERVER['PHP_SELF']."'>";
   echo "Enter your name: <input type='text' name='user_name'>";
   echo "<BR/>";
   echo "<input type='submit' value='SUBMIT NAME'>";
   echo "</form>";
 }
+
+/* $_SERVER['PHP_SELF'] reloads the current PHP page after form submission */
 ?>
