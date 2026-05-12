93dcc74701611cda4d2c8e80d17b801bf9ea2871
SDemonteverde <38735789+Shyed@users.noreply.github.com>
2026-05-12 12:36:26 -0400

    Update mac250db.sql

--- mac250db.sql
+++ mac250db.sql
@@ -1,2 +1,25 @@
+/*
+PROGRAM: MySQL User Insert Statement
+This SQL statement inserts a new user record
+into the sdemonteverdeDB.user table.
+
+FEATURES:
+- Inserts username
+- Stores encrypted password
+- Adds user description
+
+TABLE:
+sdemonteverdeDB.user
+
+FIELDS:
+- USER
+- PASSWORD
+- description
+
+NOTES:
+- Uses MySQL PASSWORD() function
+- Designed for beginner database practice
+*/
+
 INSERT INTO sdemonteverdeDB.user(USER,PASSWORD,description) 
 VALUES ('sdemonteverde', PASSWORD('sdpassword'), 'Web publisher'); 
